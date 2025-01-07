9098309e728ae81d83d569d1e2e0aaba59dbdc64
Abrilskop <116703319+Abrilskop@users.noreply.github.com>
2025-01-06 22:21:55 -0500

    feat: add variables_and_data_types.dart

---
+++ 01. variables_and_data_types.dart
@@ -0,0 +1,12 @@
+void main() {
+  // var variables
+  String mss = "Hola mundo";
+  int number = 2;
+  double costo = 5.50;
+  bool estado = true;
+  
+  print(mss);
+  print(number);
+  print(costo);
+  print(estado);
+}
